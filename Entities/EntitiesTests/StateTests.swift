03c8bf37d050f7924599e39f52bd14e0bb8ca786
Julio Guzman <julio.guzman@globant.com>
2017-10-19 18:32:34 -0500

    [BBVAChallenge] Fill State initializer tests

--- Entities/EntitiesTests/StateTests.swift
+++ Entities/EntitiesTests/StateTests.swift
@@ -12,6 +12,24 @@ import XCTest
 class StateTests: XCTestCase {
     
     func testStateJSONInitializer() {
+        guard let JSONStateFile = Bundle(for: type(of: self)).path(forResource: "State", ofType: "json"),
+              let jsonString = try? String(contentsOfFile: JSONStateFile),
+              let data = jsonString.data(using: .utf8),
+              let json = try? JSONSerialization.jsonObject(with: data) else {
+              XCTFail("JSON Initializing failed")
+              return
+        }
+        
+        let parsedObject = json as AnyObject
+        if let state = State(JSON: parsedObject) {
+            XCTAssert(state.id == 1, "State identifier initialization failed at identifier parsing")
+            XCTAssert(state.country == "USA", "State identifier initialization failed at country parsing")
+            XCTAssert(state.name == "Alabama", "State identifier initialization failed at name parsing")
+            XCTAssert(state.abbr == "AL", "State identifier initialization failed at abbreviature parsing")
+            XCTAssert(state.area == "135767SKM", "State identifier initialization failed at area parsing")
+            XCTAssert(state.largestCity == "Birmingham", "State identifier initialization failed at largest city parsing")
+            XCTAssert(state.capital == "Montgomery", "State identifier initialization failed at capital parsing")
+        }
         
     }
     
