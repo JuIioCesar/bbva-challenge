9b26a616f9529e38d5af762a7d1456609b250f9f
Julio Guzman <julio.guzman@globant.com>
2017-10-19 17:15:19 -0500

    Add cell component

---
+++ CellComponent/CellComponent/CellComponent.h
@@ -0,0 +1,19 @@
+//
+//  CellComponent.h
+//  CellComponent
+//
+//  Created by Julio Cesar Guzman Villanueva on 10/19/17.
+//  Copyright © 2017 Julio Cesar Guzman Villanueva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CellComponent.
+FOUNDATION_EXPORT double CellComponentVersionNumber;
+
+//! Project version string for CellComponent.
+FOUNDATION_EXPORT const unsigned char CellComponentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CellComponent/PublicHeader.h>
+
+
