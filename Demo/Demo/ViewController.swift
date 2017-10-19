39c880bcbbff697c4ac6f06cf30d2b217e74904e
Julio Guzman <julio.guzman@globant.com>
2017-10-19 17:18:49 -0500

    [BBVAChallenge] Add demo project to workspace

---
+++ Demo/Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by Julio Cesar Guzman Villanueva on 10/19/17.
+//  Copyright © 2017 Julio Cesar Guzman Villanueva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
